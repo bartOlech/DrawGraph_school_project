32f015d51ac6048c8c392788e70e0e9967cf0c07
Bartłomiej Olech <bartlomiejolech@MacBook-Pro-Bartomiej.local>
2020-01-23 12:51:59 +0100

    improve the function that draw a graph

--- drawGraph.py
+++ drawGraph.py
@@ -6,36 +6,41 @@ import networkx as nx
 
 import requests
 
-response = requests.get('https://drawgraph.herokuapp.com/')
+response = requests.get('https://drawgraph.herokuapp.com/?fbclid=IwAR17_cUzGZjnBaFnLbCXOGL3lX6xcrCXhyLwd4fT4zxzU9IZxPjDqqjtezM')
 resData = response.json()
 
-G = nx.Graph()
+G = nx.DiGraph()
 
-G.add_edge(str(resData[0]['importedFile']), str(resData[3]['importedFile']), weight=0.7)
-G.add_edge(str(resData[3]['importedFile']), str(resData[2]['importedFile']), weight=0.5)
-
-G.add_edge(str(resData[2]['importedFile']), str(resData[4]['importedFile']), weight=0.5)
-G.add_edge(str(resData[4]['importedFile']), str(resData[1]['importedFile']), weight=0.7)
-
+G.add_edge(str(resData[0]['importedFile']), str(resData[3]['importedFile']), weight=0)
+G.add_edge(str(resData[3]['importedFile']), str(resData[2]['importedFile']), weight=0)
+G.add_edge(str(resData[4]['importedFile']), str(resData[2]['importedFile']), weight=1)
+G.add_edge(str(resData[1]['importedFile']), str(resData[4]['importedFile']), weight=1)
 
 #
 
-elarge = [(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] > 0.5]
-esmall = [(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] <= 0.5]
+zero = [(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] == 0]
+one = [(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] == 1]
+# two = [(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] == 2]
+# three = [(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] == 3]
 
 pos = nx.spring_layout(G)  # positions for all nodes
 
 # nodes
-nx.draw_networkx_nodes(G, pos, node_size=600)
+nx.draw_networkx_nodes(G, pos, node_size=300)
 
 # edges
-nx.draw_networkx_edges(G, pos, edgelist=elarge,
-                       width=6)
-nx.draw_networkx_edges(G, pos, edgelist=esmall,
-                       width=6, alpha=0.5, edge_color='b')
+nx.draw_networkx_edges(G, pos, edgelist=zero,
+                       width=6, edge_color='g', arrowsize=30, arrowstyle='fancy')
+nx.draw_networkx_edges(G, pos, edgelist=one,
+                       width=6, edge_color='y', arrowsize=30, arrowstyle='fancy')
+# nx.draw_networkx_edges(G, pos, edgelist=two,
+#                        width=6, edge_color='y', arrowsize=30, arrowstyle='fancy')
+# nx.draw_networkx_edges(G, pos, edgelist=three,
+#                        width=6, edge_color='y', arrowsize=30, arrowstyle='fancy')
 
 # labels
 nx.draw_networkx_labels(G, pos, font_size=20, font_family='sans-serif')
 
 plt.axis('off')
 plt.show()
+
